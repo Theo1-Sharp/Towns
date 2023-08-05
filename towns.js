ba5f3ba8eaa2cd54ba9a95a4286313caad855c86
Theo1-Sharp <teodor.atanasov.24@gmail.com>
2023-08-05 06:17:14 +0300

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -1,30 +1,28 @@
-$(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
-	$('#btnAdd').click(addTown);
-	$('#btnShuffle').click(shuffleTowns);
+$(document).ready(function () {
+  $("#btnDelete").click(deleteTown);
+  $("#btnAdd").click(addTown);
+  $("#btnShuffle").click(shuffleTowns);
 });
 
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+  let townName = $("#townName").val();
+  $("#townName").val("");
+  let removed = false;
+  for (let option of $("#towns option")) {
+    if (option.textContent == townName) {
+      removed = true;
+      option.remove();
+    }
+  }
+  if (removed) showMessage(townName + " deleted.");
+  else showMessage(townName + " not found.");
 }
 
 function addTown() {
-	let townName = $('#townNameForAdd').val();
-	$('#townNameForAdd').val('');
-	$('#towns').append($('<option>').text(townName));
-	$('#result').text(townName + " added.");
+  let townName = $("#townNameForAdd").val();
+  $("#townNameForAdd").val("");
+  $("#towns").append($("<option>").text(townName));
+  $("#result").text(townName + " added.");
 }
 
 function shuffleTowns() {
@@ -41,4 +39,11 @@ function shuffleTowns() {
       array[j] = oldElement;
     }
   }
+}
+
+function showMessage(msg) {
+  $("#result").text(msg).css("display", "block");
+  setTimeout(function () {
+    $("#result").hide("blind", {}, 500);
+  }, 3000);
 }
